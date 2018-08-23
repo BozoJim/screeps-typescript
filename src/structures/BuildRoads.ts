3b87e2849f97e762227224e7e6fbd84ed2afadf1
James Stokes <JStokes7838@gmail.com>
2018-08-22 20:25:48 -0400

    Log road construction. Don't ignore creeps so roads route around them
    * This can create a parking log with a sufficient number of creeps.
      There is probably some work to be done here....

--- src/structures/BuildRoads.ts
+++ src/structures/BuildRoads.ts
@@ -7,15 +7,19 @@ export class BuildRoads {
     sources.forEach(source => {
       let path: Array<PathStep> = this.findPath(source.pos, controller.pos, room);
       for (let loc of path) {
-        room.createConstructionSite(loc.x, loc.y, STRUCTURE_ROAD);
+        if (room.createConstructionSite(loc.x, loc.y, STRUCTURE_ROAD) == 0) {
+          // log if a road is being constructed
+          console.log("Creating road at: " + room.name + ", " + loc.x + ", " + loc.y)
+        }
       }
     });
   }
 
   static findPath(from: RoomPosition, to: RoomPosition, room: Room) {
     let path: Array<PathStep> = room.findPath(from, to, {
-      ignoreCreeps: true,
-      ignoreRoads: true
+      ignoreCreeps: false,
+      ignoreRoads: true,
+      swampCost: 1
     });
     return path;
   }
